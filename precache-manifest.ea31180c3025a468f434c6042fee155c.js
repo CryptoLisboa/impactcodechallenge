9ee63c64081a09536d350f009b1bdee996b5910e
Bcfoo <>
2019-10-27 00:19:19 +0100

    Updates

---
+++ precache-manifest.ea31180c3025a468f434c6042fee155c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498a3a7d9a385fe8e5d68aab98a55908",
+    "url": "/impactcodechallenge/index.html"
+  },
+  {
+    "revision": "b9354e2dad2843226f7a",
+    "url": "/impactcodechallenge/static/css/main.27f06725.chunk.css"
+  },
+  {
+    "revision": "4ed9fbd26d1416b7bbb6",
+    "url": "/impactcodechallenge/static/js/2.ed90b6f6.chunk.js"
+  },
+  {
+    "revision": "b9354e2dad2843226f7a",
+    "url": "/impactcodechallenge/static/js/main.a032f587.chunk.js"
+  },
+  {
+    "revision": "aa154ebf27160eab5636",
+    "url": "/impactcodechallenge/static/js/runtime~main.ad52c5f4.js"
+  }
+]);
